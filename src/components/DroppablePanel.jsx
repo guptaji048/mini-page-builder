cab3daf4e04caaf185f24a9311adf40ee81a29e8
Rahul Gupta <guptaji048@gmail.com>
2021-04-22 14:21:59 +0530

    Dropzone added with modal

--- src/components/DroppablePanel.jsx
+++ src/components/DroppablePanel.jsx
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import DropModal from './DropModal';
 
 function DroppablePanel() {
+  const [draggedId, setDraggedId] = useState(null);
+
+  const handleOnDrop = (e) => {
+    const id = e.dataTransfer.getData('id');
+    setDraggedId(id);
+  };
 
   return (
     <div
       style={{ width: '100%', height: '100%' }}
+      onDragOver={(e) => {
+        e.preventDefault();
+      }}
+      onDrop={(e) => handleOnDrop(e)}
     >
       Hello
+      {draggedId && (<DropModal setDraggedId={setDraggedId} />)}
     </div>
   )
 }
 
-export default DroppablePanel;
+export default DroppablePanel
