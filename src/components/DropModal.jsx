ff4777030bb8f81b4f83c27adf477da648e4c7a0
Rahul Gupta <guptaji048@gmail.com>
2021-04-22 16:37:37 +0530

    Modal Component updated
    - Switch case function added for types of blocks.
    - Form for Label type added.

--- src/components/DropModal.jsx
+++ src/components/DropModal.jsx
@@ -1,45 +1,125 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
+import React, { useState } from 'react';
+import { Button, TextField, Typography, Dialog, DialogContent, DialogTitle, Divider, IconButton } from '@material-ui/core';
+import CloseIcon from '@material-ui/icons/Close';
 
-export default function FormDialog({ setDraggedId }) {
+export default function FormDialog({ setDroppedItemData, droppedItemData }) {
   const [open, setOpen] = React.useState(true);
 
   const handleClose = () => {
     setOpen(false);
-    setDraggedId(null);
+    setDroppedItemData(null);
   };
 
-  return (
-    <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
-      <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
-      <DialogContent>
-        <DialogContentText>
-          To subscribe to this website, please enter your email address here. We will send updates
-          occasionally.
-          </DialogContentText>
+  const LabelForm = () => {
+    const [typeDetails, setTypeDetails] = useState({
+      type: droppedItemData.type,
+      xCoord: droppedItemData.xCoord,
+      yCoord: droppedItemData.yCoord,
+      text: '',
+      fontSize: '',
+      fontWeight: '',
+    });
+    return (
+      <React.Fragment>
         <TextField
-          autoFocus
+          label='Text'
+          variant="outlined"
           margin="dense"
-          id="name"
-          label="Email Address"
-          type="email"
           fullWidth
+          style={{ marginBottom: 20 }}
+          value={typeDetails.text}
+          onChange={(e) => { setTypeDetails({ ...typeDetails, text: e.target.value }) }}
         />
+        <TextField
+          label='X'
+          variant="outlined"
+          margin="dense"
+          fullWidth
+          value={typeDetails.xCoord}
+          style={{ marginBottom: 20 }}
+        />
+        <TextField
+          label='Y'
+          variant="outlined"
+          margin="dense"
+          fullWidth
+          value={typeDetails.yCoord}
+          style={{ marginBottom: 20 }}
+        />
+        <TextField
+          label='Font Size'
+          variant="outlined"
+          margin="dense"
+          fullWidth
+          value={typeDetails.fontSize}
+          onChange={(e) => { setTypeDetails({ ...typeDetails, fontSize: e.target.value }) }}
+          style={{ marginBottom: 20 }}
+        />
+        <TextField
+          label='Font Weight'
+          variant="outlined"
+          margin="dense"
+          fullWidth
+          value={typeDetails.fontWeight}
+          onChange={(e) => { setTypeDetails({ ...typeDetails, fontWeight: e.target.value }) }}
+          style={{ marginBottom: 20 }}
+        />
+        <Button
+          style={{
+            backgroundColor: '#0044c1', color: 'white', padding: 5, textTransform: 'none', borderRadius: 3,
+          }}
+          variant="outlined"
+        >
+          Save Changes
+        </Button>
+      </React.Fragment>
+    );
+  }
+
+  const FormLoader = () => {
+    switch (droppedItemData.type) {
+      case 'Label':
+        return <LabelForm />;
+      case `Input`:
+        return null;
+      case 'Button':
+        return null;
+      default:
+        return null;
+    }
+  }
+
+  return (
+    <Dialog
+      open={open}
+      keepMounted
+      onClose={handleClose}
+      scroll="paper"
+      fullWidth
+      maxWidth="xs"
+      aria-labelledby="alert-dialog-slide-title"
+      aria-describedby="alert-dialog-slide-description"
+    >
+      <DialogTitle
+        id="alert-dialog-slide-title"
+        style={{
+          color: 'black',
+        }}
+      >
+        <div style={{
+          display: 'flex', justifyContent: 'space-between', alignItems: 'center', height: '40px',
+        }}
+        >
+          <Typography variant="h5" align="left">Edit {droppedItemData.type}</Typography>
+          <IconButton onClick={handleClose}>
+            <CloseIcon style={{ color: 'black' }} />
+          </IconButton>
+        </div>
+      </DialogTitle>
+      <Divider />
+      <DialogContent style={{ paddingTop: 20, paddingBottom: 20 }}>
+        <FormLoader />
       </DialogContent>
-      <DialogActions>
-        <Button onClick={handleClose} color="primary">
-          Cancel
-          </Button>
-        <Button onClick={handleClose} color="primary">
-          Subscribe
-          </Button>
-      </DialogActions>
     </Dialog>
   );
 }
